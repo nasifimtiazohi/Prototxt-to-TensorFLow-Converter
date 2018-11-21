789403e969c2b9fb5f8751fa72d296df137add11
nasifimtiazohi <nasifimtiaz88@gmail.com>
2018-11-21 14:57:43 -0500

    core structure based on caffe.java generated by protocol compiler from caffe.proto

---
+++ src/main/java/caffe/CodeGenerator.java
@@ -0,0 +1,39 @@
+package caffe;
+
+import java.util.HashMap;
+
+import caffe.Caffe.NetParameter;
+
+public class CodeGenerator {
+	private String simpleTensorFlowPython;
+    private String multiplexingTensorFlowPython;
+    
+    public CodeGenerator() {
+		setSimpleTensorFlowPython("");
+		setMultiplexingTensorFlowPython("");
+	}
+
+	public String getSimpleTensorFlowPython() {
+		return simpleTensorFlowPython;
+	}
+
+	private void setSimpleTensorFlowPython(String simpleTensorFlowPython) {
+		this.simpleTensorFlowPython = simpleTensorFlowPython;
+	}
+
+	public String getMultiplexingTensorFlowPython() {
+		return multiplexingTensorFlowPython;
+	}
+
+	private void setMultiplexingTensorFlowPython(String multiplexingTensorFlowPython) {
+		this.multiplexingTensorFlowPython = multiplexingTensorFlowPython;
+	}
+
+	public void generateNetworkCode(NetParameter netParameter) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
---
+++ src/main/java/core/Main.java
@@ -0,0 +1,72 @@
+package core;
+
+import caffe.Caffe;
+import caffe.Caffe.NetParameter;
+import caffe.CodeGenerator;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+import com.google.protobuf.ExtensionRegistry;
+import com.google.protobuf.TextFormat;
+import com.google.protobuf.TextFormat.ParseException;
+
+public class Main {
+	static String inputFileName="deploy.prototxt";
+	static String prototxtInput;
+	static File simpleOutput;
+	static File multiplexingOutput;
+	public static void main(String[] args) {
+		
+		//read input prototxt file
+		try {
+			prototxtInput=FileUtils.readFileToString(new File(inputFileName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("input file not found");
+			e.printStackTrace();
+		}
+		
+		//TODO: identify which type of file? deploy/solver/train_val?
+		
+		CodeGenerator codeGen= new CodeGenerator();
+		
+		
+		//if input is a deploy file containing network Info
+		NetParameter.Builder netParameterBuilder=NetParameter.newBuilder();
+		try {
+			TextFormat.merge(prototxtInput, ExtensionRegistry.getEmptyRegistry(), netParameterBuilder);
+		} catch (ParseException e) {
+			System.out.println("problem in parsing input prototxt to NetParameter object");
+			e.printStackTrace();
+		}
+		NetParameter netParameter=netParameterBuilder.build();
+		codeGen.generateNetworkCode(netParameter);
+		
+		// write code to file
+		InstantiateOutputFiles();
+		FileWriter writer;
+		try {
+			writer = new FileWriter(simpleOutput);
+			writer.write(codeGen.getSimpleTensorFlowPython());
+			writer.close();
+			writer=new FileWriter(multiplexingOutput);
+			writer.write(codeGen.getMultiplexingTensorFlowPython());
+			writer.close();
+		} catch (IOException e) {
+			System.out.println("problem in writing to output files");
+			e.printStackTrace();
+		}
+		
+	
+	}
+	private static void InstantiateOutputFiles() {
+		String filenameWithoutExtension=inputFileName.substring(0, inputFileName.length()-9);
+		simpleOutput=new File("simple"+filenameWithoutExtension+".py");
+		multiplexingOutput=new File("multiplexing"+filenameWithoutExtension+".py");
+	}
+
+}
