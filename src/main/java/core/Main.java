cc8e2cb8a66e664c71d00a550f533abba87d2245
nasifimtiazohi <nasifimtiaz88@gmail.com>
2018-12-11 18:20:07 -0500

    cleaning output files

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -23,10 +23,10 @@ public class Main {
 	public static void main(String[] args) {
 		
 		// work on given 3 input files
-		String[] myInputFiles=new String[] {"inceptionv1.prototxt","alexnet.prototxt","lenet.prototxt"};
-		for (int i=0;i<myInputFiles.length;i++) {
-			generateTensorFlowFiles(myInputFiles[i]);
-		}
+//		String[] myInputFiles=new String[] {"inceptionv1.prototxt","alexnet.prototxt","lenet.prototxt"};
+//		for (int i=0;i<myInputFiles.length;i++) {
+//			generateTensorFlowFiles(myInputFiles[i]);
+//		}
 		
 		if(args.length > 0) {
 			for(int i=0;i<args.length;i++) {
